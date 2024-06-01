be263d9f13ae2564483303c62fddfb64646b83a7
Isaac Adewumi <ienioladewumi@gmail.com>
2024-06-01 17:42:33 +0100

    add jobs to queue

--- src/pkg/bullmq/index.ts
+++ src/pkg/bullmq/index.ts
@@ -7,6 +7,25 @@ import { config } from '~/pkg/env';
 
 const queue = new Queue('supersub-queue', { connection: redis });
 
+const addJobsToQueue = async () => {
+  // Runs every 2 minutes
+  await queue.add('enrich-tokens', {}, { repeat: { pattern: '*/2 * * * *' } });
+
+  // Runs every 12 hours (0th minute of every 12th hour)
+  await queue.add('renew-subscriptions', {}, { repeat: { pattern: '0 */12 * * *' } });
+
+  // Runs every 3 minutes
+  await queue.add('fetch-smart-accounts', {}, { repeat: { pattern: '*/3 * * * *' } });
+
+  // Runs every minute
+  await queue.add('index-subscription-plugin-events', {}, { repeat: { pattern: '* * * * *' } });
+
+  // Runs every 24 hours (daily at midnight)
+  await queue.add('upcoming-subscriptions-renewal-reminders', {}, { repeat: { pattern: '0 0 * * *' } });
+};
+
+addJobsToQueue();
+
 const sandboxedWorkerPath = () => {
   return config.ENVIRONMENT === 'development'
     ? path.join(path.dirname(path.dirname(path.dirname(__filename))), 'workers', `sandboxed.ts`)
